4f790e901f78d23c80d6a46a48adb5cbeabbc870
alexander <email@alexander-bauer.com>
2016-07-15 19:16:16 +0200

    Basic feature extraction, visualizations

--- src/features/merge_power_files.py
+++ src/features/merge_power_files.py
@@ -1,8 +1,8 @@
 import sys
+
 sys.path.append("../")
 
 from utils.utils import *
-
 
 
 def parse_power(filename, dropna=True):
@@ -13,6 +13,7 @@ def parse_power(filename, dropna=True):
         df = df.dropna()
     return df
 
+
 pow_train1 = parse_power('/train_set/power--2008-08-22_2010-07-10.csv')
 pow_train2 = parse_power('/train_set/power--2010-07-10_2012-05-27.csv')
 pow_train3 = parse_power('/train_set/power--2012-05-27_2014-04-14.csv')
@@ -21,6 +22,5 @@ pow_train = pd.concat([pow_train1, pow_train2, pow_train3])
 
 pow_test = parse_power('power-prediction-sample-2014-04-14_2016-03-01.csv', False)
 
-pow_all=pd.concat([pow_train, pow_test])
-pow_all.to_pickle(config.data_folder+'/target.pkl')
-
+pow_all = pd.concat([pow_train, pow_test])
+pow_all.to_pickle(config.data_folder + '/target.pkl')
